c193a8034c91e3f92eede698f42d5fdc6c09a03d
Hadley <yeldah630@gmail.com>
2020-05-27 14:52:41 -0500

    Fixed array formatting and removed unneccessary comments: https://github.com/hadleyshapland/step-portfolio/pull/2#discussion_r431391151 and https://github.com/hadleyshapland/step-portfolio/pull/2#discussion_r431391784

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,31 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
 function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+  const greetings = [
+    'Hello world!', 
+    '¡Hola Mundo!', 
+    '你好，世界！', 
+    'Bonjour le monde!'
+  ];
 
-//   Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-//   Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
 
 function addRandomFact() {
-    const facts = 
-    ['I have 2 dogs, Olive and Bella', 'I am turning 20 on June 30th', 'I live on a farm',
+  const facts = [
+    'I have 2 dogs, Olive and Bella', 
+    'I am turning 20 on June 30th', 
+    'I live on a farm',
     'I switched my major from Biomedical Engineering to CS at the end of my first year',
-    'I have never been to the West Coast', 'My dogs have an instagram: @olive_and_bella'];
+    'I have never been to the West Coast', 
+    'My dogs have an instagram: @olive_and_bella'
+  ];
 
-    //Pick a random fact
-    const fact = facts[Math.floor(Math.random() * facts.length)];
+  const fact = facts[Math.floor(Math.random() * facts.length)];
 
-    //Add it to the page
-    const factContainer = document.getElementById('fact-container');
-    factContainer.innerText = fact;
+  const factContainer = document.getElementById('fact-container');
+  factContainer.innerText = fact;
 }
