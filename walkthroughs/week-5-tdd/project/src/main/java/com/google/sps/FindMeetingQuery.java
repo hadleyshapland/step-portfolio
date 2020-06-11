a55ea61c3e7affe2b1d89c5f4b897863fc452b33
Hadley <yeldah630@gmail.com>
2020-06-11 21:26:26 +0000

    finished algorithm, need to fix comments/clean up still

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,102 @@
 
 package com.google.sps;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Iterator;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    //TODO: explain how algorithm works & big O complexity
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        Collection<String>  attendeesRequested = request.getAttendees();
+        int meetingDuration = (int)request.getDuration();
+
+        //check for no attendees
+        if(attendeesRequested.isEmpty()) {
+            return Arrays.asList(TimeRange.WHOLE_DAY);
+        }
+
+        //check duration of meeting request
+        if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+            return Arrays.asList();
+        }
+
+        List<TimeRange> badTimes = getBadTimes(events, attendeesRequested);
+
+
+        //list to hold all the good times (everything not in badTimes)
+        List<TimeRange> goodTimes = getGoodTimes(badTimes, meetingDuration);
+
+
+        // Collections.sort(goodTimes, TimeRange.ORDER_BY_START);
+        return goodTimes;
+    }
+
+
+    private List<TimeRange> getBadTimes(Collection<Event> events,  Collection<String>  attendeesRequested) {
+        List<TimeRange> badTimes = new ArrayList<TimeRange>(); 
+
+        //loop through all events and add  time to badTimes if attendee is in attendeesRequested
+        for (Event conflictEvent: events) {
+            Set<String> busyAttendees = conflictEvent.getAttendees();
+            //while loop to see if any attendees in set are in attendeesRequested
+            //if there is overlap, add time to badTimes and stop iterating
+            Boolean conflict = false;
+            Iterator<String> eventIter = busyAttendees.iterator();
+            while(!conflict && eventIter.hasNext()) {
+                if(attendeesRequested.contains(eventIter.next())) {
+                    conflict = true;
+                }
+            }
+            if(conflict == true) {
+                badTimes.add(conflictEvent.getWhen());
+            }
+        }
+
+        //sort badTimes
+        Collections.sort(badTimes, TimeRange.ORDER_BY_START);
+
+        return badTimes;
+    }
+
+
+private List<TimeRange> getGoodTimes(List<TimeRange> badTimes, int meetingDuration) {
+
+    List<TimeRange> goodTimes = new ArrayList<TimeRange>();
+
+     int goodStart = TimeRange.START_OF_DAY;
+        int goodEnd = TimeRange.END_OF_DAY;
+
+        for(TimeRange badRange : badTimes) {
+            goodEnd = badRange.start();
+            
+            if(goodEnd > goodStart) {
+                TimeRange goodRange = TimeRange.fromStartEnd(goodStart, goodEnd, false);
+                if(goodRange.duration() >= meetingDuration) { //minimum meeting time is 30 min
+                goodTimes.add(goodRange);
+                }
+            }
+
+            //make sure nested events are handled correctly
+            if(goodStart < (goodEnd + badRange.duration())) {
+                goodStart = goodEnd + badRange.duration();
+            }
+            
+        }
+
+        //edge case for last time chunk
+        if (goodStart < TimeRange.END_OF_DAY) {
+            TimeRange goodRange = TimeRange.fromStartEnd(goodStart, TimeRange.END_OF_DAY, true);
+            if(goodRange.duration() >= meetingDuration) {
+                goodTimes.add(goodRange);
+                }
+        }
+
+        return goodTimes;
+}
 }
