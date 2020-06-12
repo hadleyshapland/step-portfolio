6f9303d2d14e3dc39273614b687e5501ce78b6ef
Hadley <yeldah630@gmail.com>
2020-06-12 18:00:08 +0000

    scenario 4 test

--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -414,6 +414,44 @@ public final class FindMeetingQueryTest {
     Assert.assertEquals(expected, actual);
   }
 
+    @Test
+    public void allOptional() {
+        // Scenario 4: No mandatory atendees, just two optional attendees with gaps in their schedules.
+        // These gaps should be identified and returned.
+
+        Collection<Event> events =
+            Arrays.asList(
+                new Event(
+                    "Event 1",
+                    TimeRange.fromStartDuration(TIME_0800AM, DURATION_30_MINUTES),
+                    Arrays.asList(PERSON_A)),
+                new Event(
+                    "Event 2",
+                    TimeRange.fromStartDuration(TIME_0900AM, DURATION_30_MINUTES),
+                    Arrays.asList(PERSON_B)),
+                new Event(
+                    "Event 3",
+                    TimeRange.fromStartDuration(TIME_1000AM, DURATION_1_HOUR),
+                    Arrays.asList(PERSON_B)));
+
+        MeetingRequest request =
+            new MeetingRequest(Arrays.asList(), DURATION_30_MINUTES);
+
+        request.addOptionalAttendee(PERSON_A);
+        request.addOptionalAttendee(PERSON_B);
+
+        Collection<TimeRange> actual = query.query(events, request);
+        Collection<TimeRange> expected =
+            Arrays.asList(
+                TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0800AM, false),
+                TimeRange.fromStartEnd(TIME_0830AM, TIME_0900AM, false),
+                TimeRange.fromStartEnd(TIME_0930AM, TIME_1000AM, false),
+                TimeRange.fromStartEnd(TIME_1100AM, TimeRange.END_OF_DAY, true));
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
 
 
 
