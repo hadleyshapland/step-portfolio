6ea1c131e7c3c0bed522f7bf099b87bf7d282796
Hadley Shapland <hadley.a.shapland@vanderbilt.edu>
2020-08-06 17:01:07 -0500

    update host & delete for fun section

--- portfolio/src/main/webapp/js/scripts.js
+++ portfolio/src/main/webapp/js/scripts.js
@@ -1,6 +1,3 @@
-//global variable for map
-let map;
-
 /*!
     * Start Bootstrap - Resume v6.0.0 (https://startbootstrap.com/template-overviews/resume)
     * Copyright 2013-2020 Start Bootstrap
@@ -46,7 +43,6 @@ let map;
 
 //called in body onload
 function onLoad() {
-    createMap();
     loadComments();
 }
 
@@ -92,143 +88,4 @@ function deleteComment(id) {
   const params = new URLSearchParams();
   params.append('id', id);
   fetch('/delete-comment', {method: 'POST', body: params});
-}
-
-//google map API
-function createMap() {
-  const darkModeArray = getDarkModeArray();
-  
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 36.1486, lng: -86.8050}, 
-    zoom: 2,
-    styles: darkModeArray,
-  });
-}
-
-function codeAddress(address) {
-  let goTo = address;
-  if(goTo == undefined) {
-    goTo = document.getElementById('address').value;
-  }
-
-  let geocoder = new google.maps.Geocoder();
-  geocoder.geocode( { 'address': goTo}, function(results, status) {
-    if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
-      map.setZoom(15);
-      const marker = new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location,
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
-
-//dark mode styles from https://developers.google.com/maps/documentation/javascript/styling?hl=en_US
-function getDarkModeArray() {
-return [
-  {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-  {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-  {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-  {
-    featureType: 'administrative.locality',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'geometry',
-    stylers: [{color: '#263c3f'}]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#6b9a76'}]
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [{color: '#38414e'}]
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.stroke',
-    stylers: [{color: '#212a37'}]
-  },
-  {
-    featureType: 'road',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#9ca5b3'}]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry',
-    stylers: [{color: '#746855'}]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [{color: '#1f2835'}]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#f3d19c'}]
-  },
-  {
-    featureType: 'transit',
-    elementType: 'geometry',
-    stylers: [{color: '#2f3948'}]
-  },
-  {
-    featureType: 'transit.station',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}]
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [{color: '#17263c'}]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#515c6d'}]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.stroke',
-    stylers: [{color: '#17263c'}]
-  }];
-}
-
-//geo chart API
-google.charts.load('current', {
-  'packages':['geochart'],
-  'mapsApiKey': 'AIzaSyBd6L5aVm5z-MWG4eBEiG1V4WF0pBn7LiM'
-});
-
-google.charts.setOnLoadCallback(drawRegionsMap);
-
-function drawRegionsMap() {
-  fetch('/region-data').then(response => response.json())
-  .then((regionVotes) => {
-    const data = new google.visualization.DataTable();
-    data.addColumn('string', 'Country');
-    data.addColumn('number', 'Votes');
-    Object.keys(regionVotes).forEach((region) => {
-      data.addRow([region, regionVotes[region]]);
-    });
-    const options = {};
-    let chart = new google.visualization.GeoChart(document.getElementById('chart-container'));
-
-    chart.draw(data, options);
-  });
 }
